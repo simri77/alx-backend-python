90f9647c7e589f26905616b3b2b9db54feb80ca9
simri77 <simretmesfin97@gmail.com>
2023-01-06 07:17:51 -0800

    Basic annotations - add

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,3 +1,7 @@
-#!usr/bin/env python3
+#!/usr/bin/env python3
+""" annotations for adding two floats"""
+
+
 def add(a: float, b: float) -> float:
+    """ Returns the sum of the two floats"""
     return a + b
